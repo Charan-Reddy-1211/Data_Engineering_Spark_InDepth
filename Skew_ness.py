ae7249c2bde52159fba0efbf94a5bf600d85de1f
Charan-Reddy-1211 <charanreddyb1211@gmail.com>
2025-12-02 11:21:54 +0530

    Skew Test

--- Skew_ness.py
+++ Skew_ness.py
@@ -1,24 +1,19 @@
+from pyspark.sql import SparkSession
+from pyspark.sql.functions import lit
+
+spark = SparkSession.builder.getOrCreate()
+spark.conf.set("spark.sql.adaptive.enabled", "false")
+
+# 10 million rows with SAME key
+big = spark.range(0, 10000000).withColumn("id", lit(1))
+
+# small table
+small = spark.range(0, 10).withColumn("id", lit(1))
+
+df = big.join(small, "id")
+
+print(df.count())
 
 
-from Utils.common import  create_spark_session
-from pyspark.sql.functions import *
-app_name='skew'
-
-spark=create_spark_session(app_name)
-
-schema = ["id"]
-data = [("1",)]*1000000
-data2=[("2",)]*10
-one_data = spark.createDataFrame(data, schema)
-
-two_data = spark.createDataFrame(data2, schema)
-
-union_df=one_data.union(two_data)
-print(union_df.columns)
-union_df=union_df.withColumn("partitionID",spark_partition_id())
-union_df_grouped = union_df.groupBy("id","partitionID").count()
-union_df_grouped.show()
-
-import  time
+import time
 time.sleep(500)
-spark.stop()
